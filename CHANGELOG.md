1ef544c79f8f53f3e7cd6f3cab04eba60db7e869
Andrea Mazzini <andrea.mazzini@gmail.com>
2016-03-15 10:22:12 +0100

    Add Carthage support

---
+++ CHANGELOG.md
@@ -0,0 +1,13 @@
+# Change Log
+All notable changes to this project will be documented in this file.
+`SubtleVolume` adheres to [Semantic Versioning](http://semver.org/).
+
+- `0.1.x` Releases - [0.1.0](#010) 
+
+---
+
+## [0.1.0](https://github.com/andreamazz/SubtleVolume/releases/tag/0.1.0)
+
+###Added
+- Carthage Support 
+
---
+++ SubtleVolume/SubtleVolume.h
@@ -0,0 +1,19 @@
+//
+//  SubtleVolume.h
+//  SubtleVolume
+//
+//  Created by Andrea Mazzini on 15/03/16.
+//  Copyright © 2016 Andrea Mazzini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SubtleVolume.
+FOUNDATION_EXPORT double SubtleVolumeVersionNumber;
+
+//! Project version string for SubtleVolume.
+FOUNDATION_EXPORT const unsigned char SubtleVolumeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SubtleVolume/PublicHeader.h>
+
+
